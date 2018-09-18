0fbaa0f1e40200ed8ddc502ba4ed5fbf3c95e7c1
cobrien1 <cobrien1@LAPTOP-ET95LRJ5>
2018-09-18 16:26:51 +1000

    Add project files.

---
+++ WebHotel/Models/Booking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebHotel.Models
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public int RoomID { get; set; }
+        public string CustomerEmail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Cost { get; set; }
+        public ICollection<Room> TheRoom { get; set; }
+        public ICollection<Customer> TheCustomer { get; set; }
+
+    }
+}
---
+++ WebHotel/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebHotel.Models
+{
+    public class Customer
+    {
+        [Key, Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z-']")]
+        [Range(2,20)]
+        public string Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z-']")]
+        [Range(2, 20)]
+        public string GivenName { get; set; }
+
+        [Required]
+        [RegularExpression(@"{4}")]
+        public string Postcode { get; set; }
+        public ICollection<Booking> TheBookings { get; set; }
+    }
+}
